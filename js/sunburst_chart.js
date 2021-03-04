3d9934dce7960caba95b937b11a92d555bd3cb28
doublealogic <cras.ajrobertson@gmail.com>
2021-03-03 22:36:18 -0500

    Updated fungi kindgom page

--- js/sunburst_chart.js
+++ js/sunburst_chart.js
@@ -1,3 +1,35 @@
+var data;
+
+d3.json('mushroom_list.json').then(jsonData => {
+        console.log(jsonData);
+        data = jsonData
+        })
+
+partition = data => {
+    const root = d3.hierarchy(data)
+        .sum(d => d.value)
+        .sort((a, b) => b.value - a.value);
+    return d3.partition()
+        .size([2 * Math.PI, root.height + 1])
+        (root);
+    }
+
+color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+
+format = d3.format(",d")
+
+width = 932
+
+radius = width / 6
+
+arc = d3.arc()
+    .startAngle(d => d.x0)
+    .endAngle(d => d.x1)
+    .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+    .padRadius(radius * 1.5)
+    .innerRadius(d => d.y0 * radius)
+    .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
+
 const root = partition(data);
 
 root.each(d => d.current = d);
@@ -87,33 +119,3 @@ function labelTransform(d) {
     return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
 }
 
-    return svg.node();
-
-data = FileAttachment("mushroom_list.json").json()
-
-partition = data => {
-const root = d3.hierarchy(data)
-    .sum(d => d.value)
-    .sort((a, b) => b.value - a.value);
-return d3.partition()
-    .size([2 * Math.PI, root.height + 1])
-    (root);
-}
-
-color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
-
-format = d3.format(",d")
-
-width = 932
-
-radius = width / 6
-
-arc = d3.arc()
-    .startAngle(d => d.x0)
-    .endAngle(d => d.x1)
-    .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
-    .padRadius(radius * 1.5)
-    .innerRadius(d => d.y0 * radius)
-    .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
-
-d3 = require("d3@6")
